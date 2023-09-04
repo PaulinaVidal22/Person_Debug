10f063f4d744d4649d2511e92cfbacd2a600a4c5
PauloinaVidal22 <paulina.vidal@correo.ucu.edu.uy>
2023-09-04 10:49:21 -0300

    corrigiendo errores, probando el debug

--- src/Library/Person.cs
+++ src/Library/Person.cs
@@ -42,7 +42,7 @@ namespace Ucu.Poo.Persons
         {
             get
             {
-                return $"{this.Name}{this.FamilyName}";
+                return $"{this.Name} {this.FamilyName}";
             }
         }
 
@@ -66,7 +66,7 @@ namespace Ucu.Poo.Persons
                     age--;
                 }
 
-                return age;
+                return age * -1;
             }
         }
 
